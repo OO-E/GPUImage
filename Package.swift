a79e4d7d676ad8112758355c7bba4c8d4136a13e
Orhan Özgün Ergen <>
2023-03-24 15:33:05 +0300

    bug fix

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(name: "GPUImage",
                                           targets: ["GPUImage"])],
                       targets: [.target(name: "GPUImage",
                                         path: "framework/Source",
-                                        publicHeadersPath: "")]
+                                        publicHeadersPath: "")],
                       exclude: ["framework/Source/iOS",
                         "framework/Source/GPUImageFilterPipeline.*",
                         "framework/Source/GPUImageMovieComposition.*",
