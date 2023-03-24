35b681b7bccf0f0d3e3cc78790129ed5b7086f6d
Orhan Özgün Ergen <>
2023-03-24 15:03:58 +0300

    bg fix

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(name: "GPUImage",
                       platforms: [.macOS(.v10_10),
-                                  .iOS(.v9),
+                                  .iOS(.v11),
                                   .tvOS(.v9),
                                   .watchOS(.v2)],
                       products: [.library(name: "GPUImage",
